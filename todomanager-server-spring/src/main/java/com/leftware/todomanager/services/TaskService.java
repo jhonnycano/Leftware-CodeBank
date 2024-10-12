8d685afd5d0f7b86c3277a3ea159bc7f890b83a1
Jhonny D. Cano <jhonnycano@hotmail.com>
2024-10-12 10:03:34 -0500

    todomanager: WIP change status

--- todomanager-server-spring/src/main/java/com/leftware/todomanager/services/TaskService.java
+++ todomanager-server-spring/src/main/java/com/leftware/todomanager/services/TaskService.java
@@ -1,6 +1,7 @@
 package com.leftware.todomanager.services;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,17 +13,41 @@ import com.leftware.todomanager.models.Task;
 public class TaskService {
 
     private final List<Task> tasks;
+    private final List<String> statuses;
 
     public TaskService() {
         this.tasks = new ArrayList<>();
         this.tasks.add(new Task(UUID.randomUUID().toString(), "123", "Tarea", "Pending", null));
+        this.statuses = Arrays.asList("Pending", "In Progress", "Done");
     }
 
     public List<Task> getTasksByProjectId(String projectId) {
         return tasks;
     }
 
+    public Task getTaskById(String projectId, String taskId) {
+        Task task = tasks.stream()
+                .filter(p -> (p.getProjectId().equals(projectId)) && p.getId().equals(taskId))
+                .findFirst()
+                .orElse(null);
+        return task;
+    }
+
     public void addTask(Task task) {
         tasks.add(task);
+    }
+
+    public String changeStatus(String projectId, String taskId, String status) {
+        Task task = getTaskById(projectId, taskId);
+        if (task == null) {
+            return "Task not found";
+        }
+
+        if (!statuses.contains(status)) {
+            return "Invalid status";
+        }
+
+        task.setStatus(status);
+        return null;
     }
 }
